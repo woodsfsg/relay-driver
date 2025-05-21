fe2b764a24f6c823105a32bbd0d27f6b266ee5a8
woodsfsg <130228546+woodsfsg@users.noreply.github.com>
2025-05-21 15:37:30 +0100

    Update __init__.py

--- components/max4820/switch/__init__.py
+++ components/max4820/switch/__init__.py
@@ -2,7 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
 from esphome.const import (
-  conf_id,
+  CONF_ID,
+  CONF_NUMBER,
 )
 
 from .. import max4820_ns MAX4820
@@ -10,13 +11,15 @@ from .. import max4820_ns MAX4820
 DEPENDENCIES = ["max4820"]
 
 MAX4820Switch = max4820_ns.class_("MAX4820Switch", switch.Switch, cg.Component)
+CONF_MAX4820_ID = "max4820_id"
+CONF_SWITCH_OUTPUT = "output"
 
 CONFIG_SCHEMA = (
   switch.switch_schema(MAX4820Switch)
   .extend(
       {
-          cv.GenerateID(CONF_MAX4820_ID): cv.use(MAX4820),
-          cv.Required(CONF_SWITCH_OUTPUT): cv.int_range(min=0, max=7),
+          cv.GenerateID(CONF_MAX4820_ID): cv.use_id(MAX4820),
+          cv.Required(CONF_NUMBER): cv.int_range(min=0, max=7),
       }
   )
   .extend(cv.COMPONENT_SCHEMA)
