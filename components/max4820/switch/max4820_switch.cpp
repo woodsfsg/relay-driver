c80e8b47fefb1f43c2b2fb94d141b2ea04741b96
woodsfsg <130228546+woodsfsg@users.noreply.github.com>
2025-05-21 17:03:05 +0100

    Create max4820_switch.cpp

---
+++ components/max4820/switch/max4820_switch.cpp
@@ -0,0 +1,26 @@
+#include "esphome/core/log.h"
+#include "max4820_switch.h"
+
+namespace esphome {
+namespace max4820 {
+
+static const char *const TAG = "max4820.switch";
+
+void MAX4820Switch::setup() {
+  bool state = this->parent_->get_switch_state(this->switch_id_);
+  this->publish_state(state);
+}
+
+void MAX4820Switch::write_state(bool state) {
+  ESP_LOGV(TAG, "Setting switch %u: %s", this->switch_id_, ONOFF(state));
+  this->parent_->set_switch_value(this->switch_id_, state);
+  this->publish_state(state);
+}
+
+void MAX4820Switch::dump_config() {
+  LOG_SWITCH("", "MAX4820 Switch", this);
+  ESP_LOGCONFIG(TAG, "  Switch has ID %u", this->switch_id_);
+}
+
+}  // namespace max4820
+}  // namespace esphome
