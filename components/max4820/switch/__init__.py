430059cc07e74b999ccc5f6d0f3f427795ce5eb9
woodsfsg <130228546+woodsfsg@users.noreply.github.com>
2025-05-21 15:43:24 +0100

    Update __init__.py

--- components/max4820/switch/__init__.py
+++ components/max4820/switch/__init__.py
@@ -24,3 +24,12 @@ CONFIG_SCHEMA = (
   )
   .extend(cv.COMPONENT_SCHEMA)
 )
+
+async def to_code(config):
+  var = await switch.new_switch(config)
+  await cg.register_component(var, config)
+
+  parent_ = await cg.get_variable(config[CONF_MAX4820_ID])
+  cg.add(var.set_max4820_parent(parent_))
+
+  cg.add(var.set_switch_id(config[CONF_SWITCH_OUTPUT])
