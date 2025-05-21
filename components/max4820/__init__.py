59538bdb87c5b61314b444df4f6f8c07120663bd
woodsfsg <130228546+woodsfsg@users.noreply.github.com>
2025-05-21 12:23:17 +0100

    Update __init__.py

--- components/max4820/__init__.py
+++ components/max4820/__init__.py
@@ -1,7 +1,6 @@
 from esphome import pins
 import esphome.codegen as cg
 from esphome.components import spi
-from esphome.components import switch
 import esphome.config_validation as cv
 from esphome.const import (
     CONF_CLOCK_PIN,
@@ -17,10 +16,6 @@ MULTI_CONF = True
 max4820_ns = cg.esphome_ns.namespace("max4820")
 
 MAX4820Component = max4820_ns.class_("MAX4820Component", spi.SPIDevice, cg.Component)
-
-MAX4820Switch = max4820_ns.class_(
-    "MAX4820Switch", switch.Switch, cg.Parented.template(MAX4820Component)
-)
 
 CONF_MAX4820 = "max4820"
 CONF_SET_PIN = "set_pin"
@@ -42,29 +37,5 @@ async def to_code(config):
     await spi.register_spi_device(var, config)
 
     cg.add(var.set_sr_count(config[CONF_SR_COUNT]))
-
-
-MAX4820_SWITCH_SCHEMA = switch.switch_schema(
-    MAX4820Switch,
-    block_inverted=False,
-).extend(
-    {
-        cv.Required(CONF_MAX4820): cv.use_id(MAX4820Component),
-        cv.Optional(CONF_NUMBER, default=1): cv.int_range(min=0, max=31),
-    }
-)
-
-
-def max4820_switch_final_validate(relay_config, parent_config):
-    max_relay = parent_config[CONF_SR_COUNT] * 8
-    if relay_config[CONF_NUMBER] >= max_switch:
-        raise cv.Invalid(f"Relay number must be less than {max_relay}")
-
-
-async def max4820_switch_to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_parented(var, config[CONF_MAX4820])
-
-    cg.add(var.set_number(config[CONF_NUMBER]))
-    cg.add(var.set_inverted(config[CONF_INVERTED]))
-    return var
+    cg.add(var.set_set_pin(config[CONF_SET_PIN]))
+    cg.add(var.set_reset_pin(config[CONF_RESET_PIN]))
