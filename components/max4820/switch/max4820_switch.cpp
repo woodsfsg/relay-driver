3b32c96e72c43bec5a0893dd9a49c35dc4b5cf93
woodsfsg <130228546+woodsfsg@users.noreply.github.com>
2025-05-21 17:26:11 +0100

    Update max4820_switch.cpp

--- components/max4820/switch/max4820_switch.cpp
+++ components/max4820/switch/max4820_switch.cpp
@@ -7,19 +7,19 @@ namespace max4820 {
 static const char *const TAG = "max4820.switch";
 
 void MAX4820Switch::setup() {
-  bool state = this->parent_->get_switch_state(this->switch_id_);
+  bool state = this->parent_->get_switch_state(this->switch_output_);
   this->publish_state(state);
 }
 
 void MAX4820Switch::write_state(bool state) {
-  ESP_LOGV(TAG, "Setting switch %u: %s", this->switch_id_, ONOFF(state));
-  this->parent_->set_switch_state(this->switch_id_, state);
+  ESP_LOGV(TAG, "Setting switch %u: %s", this->switch_output_, ONOFF(state));
+  this->parent_->set_switch_state(this->switch_output_, state);
   this->publish_state(state);
 }
 
 void MAX4820Switch::dump_config() {
   LOG_SWITCH("", "MAX4820 Switch", this);
-  ESP_LOGCONFIG(TAG, "  Switch has ID %u", this->switch_id_);
+  ESP_LOGCONFIG(TAG, "  Switch has ID %u", this->switch_output_);
 }
 
 }  // namespace max4820
