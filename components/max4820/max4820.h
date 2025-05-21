6c69f8cf383567618b4f8873e90cc28e3acdd5bd
woodsfsg <130228546+woodsfsg@users.noreply.github.com>
2025-05-21 15:52:48 +0100

    Create max4820.h

---
+++ components/max4820/max4820.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "esphome/components/spi/spi.h"
+#include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+
+namespace esphome {
+namespace max4820 {
+
+class MAX4820 : public Component,
+                public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING,
+                                      spi::DATA_RATE_75KHZ> {  // Running at the slowest max speed supported by the
+                                                               // max4820. 2.7v = 75ksps
+ public:
+  void setup() override;
+  void dump_config() override;
+  float get_setup_priority() const override;
+};
+
+}  // namespace max4820
+}  // namespace esphome
