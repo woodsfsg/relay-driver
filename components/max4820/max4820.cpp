38e74942f30e5830360fc95c576d3aed9bb4eb4a
woodsfsg <130228546+woodsfsg@users.noreply.github.com>
2025-11-11 11:44:46 +0000

    Implement device reset pin

--- components/max4820/max4820.cpp
+++ components/max4820/max4820.cpp
@@ -12,13 +12,31 @@ float MAX4820::get_setup_priority() const { return setup_priority::BUS; }
 
 void MAX4820::setup() {
   ESP_LOGCONFIG(TAG, "Setting up max4820");
-  this->spi_setup();
-
   this->states = 0;
+  
+  this->spi_setup();
+  
+  if (this->reset_pin_ != nullptr) {
+    this->reset_pin_->setup();
+    this->reset_device();
+  }
   
   this->enable();
   this->write_byte(states);
   this->disable();
+}
+
+void MAX4820::reset_device_() {
+  this->states = 0;
+  if (this->reset_pin_ != nullptr) {
+    ESP_LOGD(TAG, "Reset device using RESET pin");
+    this->reset_pin_->digital_write(false);
+    delay(1);
+    this->reset_pin_->digital_write(true);
+  } else {
+    ESP_LOGD(TAG, "Reset device using SWRST command");
+    this->set_switch_state(0,false);
+  }
 }
 
 void MAX4820::dump_config() {
